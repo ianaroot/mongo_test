a54130b52db2400d9780c9851ea0ab8a19923cc0
ian root <ianaroot@gmail.com>
2013-12-17 00:13:26 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MongoTest::Application.config.secret_token = '2383e0d7249c5f1dbcb7c83cf8d5db0bd68722e1fd192f093e3f6feca0c4182db71b121b5d7d96067352925b7cb092efcea22d3c8d02361372bf4acabe472472'
